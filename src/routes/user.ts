fcce2b6be7b13bd12d88d8e56b67b948d3fae603
Kody Smith <smithk@versifit.com>
2020-08-25 22:11:28 -0500

    User session handles error/expiration of discord token a bit better

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,7 @@ import { DefaultState, Context, ParameterizedContext } from 'koa';
 import { DiscordApi } from "../api-wrappers/discord-api";
 
 import { FESession } from "../models/FESession";
+import { APIUser } from "discord-api-types/default";
 
 // Needed for context type shenanigans
 export type RContext = ParameterizedContext<DefaultState, Context & Router.RouterParamContext<DefaultState, Context>>;
@@ -10,11 +11,17 @@ export type RContext = ParameterizedContext<DefaultState, Context & Router.Route
 const routerOpts: Router.RouterOptions = {prefix: '/user'};
 const userRouter: Router = new Router<DefaultState, Context>(routerOpts);
 userRouter.get('/', async (ctx: RContext) => {
+    let user: APIUser;
+    // Attempt to resolve the discord user if we have an access token
     const oauthGrant = ctx.session.grant;
-    // TODO Probably have to handle case of this access token being expired
-    const getUser = await DiscordApi.getUser(oauthGrant.response.access_token);
-    const discordUser = await getUser.json();
-    ctx.ok(new FESession(discordUser))
+    if (oauthGrant) {
+        const getUser = await DiscordApi.getUser(oauthGrant.response.access_token);
+        if (getUser.ok) {
+            user = await getUser.json();
+        }
+        // TODO Instead of making them click login again can probably refresh token if it was a 401
+    }
+    ctx.ok(new FESession(user))
 });
 
 export default userRouter.routes();
