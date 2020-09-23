0d7e9b055691a4cecfb3190e06dff790cc64ac5a
Kody Smith <smithk@versifit.com>
2020-09-22 20:55:29 -0500

    Made type orm connection done via url

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,15 @@
 let fileExt = process.env.NODE_ENV === 'development' ? 'ts' : 'js';
 let srcDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist'
 let config = {
-   "type": "postgres",
-   "host": process.env.POSTGRES_HOST,
-   "port": 5432,
-   "username": process.env.POSTGRES_USERNAME,
-   "password": process.env.POSTGRES_PASSWORD,
-   "database": process.env.POSTGRES_DATABASE,
-   "synchronize": true,
-   "logging": false,
+    "type": "postgres",
+    "url": process.env.DB_URL,
+    "extra": {
+        "ssl": {
+            "rejectUnauthorized": false
+        }
+    },
+    "synchronize": true,
+    "logging": false,
     "entities": [
         srcDir + "/repository/entity/**/*." + fileExt
     ],
@@ -22,9 +23,6 @@ let config = {
         "entitiesDir": srcDir + "/repository/entity",
         "migrationsDir": srcDir + "/repository/migration",
         "subscribersDir": srcDir + "/repository/subscriber"
-    },
-    "driver_extra": {
-        "rejectUnauthorized": false
     }
 };
 module.exports = config;
